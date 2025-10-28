faafc4eed73fa481d1d7a854d336912d713fbf37
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-10-28 21:33:18 +0530

    Integrated Kafka || Added Activity model for deserialization

---
+++ src/main/java/com/fitness/AiService/models/ActivityType.java
@@ -0,0 +1,26 @@
+package com.fitness.AiService.models;
+
+public enum ActivityType {
+    RUNNING,
+    CYCLING,
+    SWIMMING,
+    WALKING,
+    YOGA,
+    STRENGTH_TRAINING,
+    DANCE,
+    PILATES,
+    CARDIO,
+    RESISTANCE_TRAINING,
+    MEDITATION,
+    SPORTS,
+    MARTIAL_ARTS,
+    JUMP_ROPE,
+    JUDO,
+    BOXING,
+    SKIING,
+    SNOWBOARDING,
+    CLIMBING,
+    MOUNTAIN_BIKING,
+    BIKING,
+    OTHER
+}
